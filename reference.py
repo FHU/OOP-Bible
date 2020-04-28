1af01e44b0f6475c5d179f5a86df4e1f4185cabb
Kenan Casey <kenancasey@gmail.com>
2020-04-28 15:03:17 -0500

    Update reference.py
    Add example of str in Reference.

--- reference.py
+++ reference.py
@@ -5,7 +5,14 @@ class Reference:
         self.chapter = chapter
         self.verse = verse
 
-
-
+    # TODO
     def __str__(self):
+        '''
+            Return a string representation of the Reference object.
+
+            Return the book, chapter number, verse number, and verse text. 
+
+            Example: 
+                `Genesis 50:1 And Joseph fell upon his father's face, and wept upon him, and kissed him.`
+        '''
         pass
